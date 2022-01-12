81e62c14a6d1232219b1ed9c36f629c0b80cca78
Krzysztof Banek <krisbanek@gmail.com>
2022-01-12 22:40:39 +0100

    Add create_series additoinal task & mentor recommendations

--- biblioteka_filmow.py
+++ biblioteka_filmow.py
@@ -1,4 +1,4 @@
-
+#*********************** Clases:
 class Movies:
    def __init__(self, title, year, genre):
        self.title = title
@@ -45,6 +45,58 @@ class Series:
        self._views = value
 
 
+#************************** functions:
+
+def get_movies(library):
+    for n in library:
+        if isinstance(n, Movies):
+            print(n)
+
+
+def get_series(library):
+    for n in library:
+        if isinstance(n, Series):
+            print(n) 
+
+
+def search(q):
+    for a in library:
+        if q == a.title:
+            print(a)
+
+
+import random
+def generate_views(library):
+    r = random.choice(library)
+    r.views =+ random.randint(1,101)
+    print(f"{r} views: {r.views}")
+     
+
+def ten_generate_views():
+    for i in range(10):
+        generate_views(library)
+
+top_titles_dict = {}
+def top_titles(l):
+    for a in library:
+        top_titles_dict[f"{a}"] = a.views
+    top_titles_list = sorted(top_titles_dict.items(), key = lambda x: x[1], reverse=True)
+    return print(top_titles_list[0:l])
+
+def create_series(a):
+    s_list=[]
+    b = a + 1 
+    for a in range(1,b):
+       serie = f"test_{a}"
+       serie = Series(title=t, year=y, genre=g, episode=str(a).zfill(2), season=str(s).zfill(2))
+       list.append(serie)
+       print(serie)
+    print(s_list)
+
+
+
+#***************** variables:
+
 bluesbrothers = Movies(title="The Blues Brothers", year="1980", genre="Road Movie")
 greenbook = Movies(title="The Green Book", year="2020", genre="Road Movie") 
 diune = Movies(title="Diune", year="2021", genre="Science Fiction") 
@@ -60,54 +112,31 @@ simpsons24 = Series(title="The Simpsons", year="1998", genre="Animated", episode
 
 library = [bluesbrothers, greenbook, diune, diehard, simpsons11, simpsons12, simpsons13, simpsons14, simpsons21, simpsons22, simpsons23, simpsons24]
 
-def get_movies():
-    for n in library:
-        if isinstance(n, Movies):
-            print(n)
+#*************** functions if __name__ == '__main__' for search & top_titles:
 
-
-def get_series():
-    for n in library:
-        if isinstance(n, Series):
-            print(n) 
-
-
-def search(q):
-    for a in library:
-        if q == a.title:
-            print(a)
-if __name__ == "__main__":
+if __name__ == '__main__':
     q = input("Podaj proszę szukany tytuł: ")
-    search(q)
-
-
-import random
-def generate_views():
-    r = random.choice(library)
-    r.views =+ random.choice(range(1,101))
-    print(f"{r} views: {r.views}")
-     
-
-def ten_generate_views():
-    for i in range(10):
-        generate_views()
-
-top_titles_dict = {}
-def top_titles():
-    for a in library:
-        top_titles_dict[f"{a}"] = [a.views]
-    top_titles_list = sorted(top_titles_dict.items(), key = lambda x: x[1], reverse=True)
-    return print(top_titles_list[0:l])
-if __name__ == "__main__":
+    search(q)    
+if __name__ == '__main__':
     l = int(input("Podaj proszę wybraną ilość najpopularniejszych tytułów: "))
-    top_titles()
+    top_titles(l)
 
+if __name__ == '__main__':
+    t = input("Podaj tytuł serialu: ")
+    y = int(input("Podaj rok produkcji: "))
+    g = input("Podaj gatunek: ")
+    a = int(input("Podaj ilość odcinków: "))
+    s = int(input("Podaj numer serii: "))
+    create_series(a)
 
+#**************** functions executive:
 
 print("Biblioteka filmów")
-get_movies()
-get_series()
+search(q)
+get_movies(library)
+get_series(library)
 ten_generate_views()
 import time
 print("Najpopularniejsze filmy i seriale dnia",time.strftime("%d:%m:%Y", time.localtime()))
-top_titles()
+top_titles(l)
+create_series(a)
